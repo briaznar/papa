bafc57cd02909850550c3ad5cab16be817874030
briaznar <brigy19@gmail.com>
2016-11-15 11:00:38 +0100

    tasks 1 and 2

--- assignment3_working/bri/papa_assignment3_2_client.py
+++ assignment3_working/bri/papa_assignment3_2_client.py
@@ -3,13 +3,12 @@ import json
 def Main():
     host = 'localhost'
     port = 8080
-
-    print('Sending http://www.example.com:80/path/to/myfile.html?key1=value1&key2=value2#InTheDocument to the server')
+    url  = "http://www.example.com:80/path/to/myfile.html?key1=value1&key2=value2#InTheDocument\\r\\n"
+    print('Sending {}', url)
 
     clientSocket = socket.socket()
     clientSocket.connect((host, port))
 
-    url = "http://www.example.com:80/path/to/myfile.html?key1=value1&key2=value2#InTheDocument\\r\\n"
     clientSocket.send(url.encode('utf-8'))
 
     clientSocket.close()
