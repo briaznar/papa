2fd613a2a91d0d955bcbe7c07cbf5ff3223ff6d6
briaznar <brigy19@gmail.com>
2016-11-21 01:47:26 +0100

    deletes output files, add comments, small modifications

--- assignment4_working/papa_dowloadEverything.py
+++ assignment4_working/papa_dowloadEverything.py
@@ -1,12 +1,15 @@
 from urllib.parse import urlparse
+#call to the http client program
 from  papa_http_client import download_files
 import re
 
 def get_images_path(file_name,url):
     images = []
     image_path = []
+    #regex to check the image tag
     regex = re.compile(r'<\s*img [^>]*src="([^"]+)')
     url = (urlparse(url))
+    #gets prefix to absolute path
     host = url.scheme + "://" + url.netloc
 
     with open(file_name) as file:
@@ -15,6 +18,7 @@ def get_images_path(file_name,url):
                 images.append(regex.findall(line))
     for img in images:
         for i in img:
+            # homogenizes relative paths to absolute
             if i.find("http://") == -1:
                 i = host + i
             image_path.append(i)
